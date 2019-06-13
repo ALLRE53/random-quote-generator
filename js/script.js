c65362a4cb61ac5382016a2dd687954da4d01b5d
ALLRE53 <51243758+ALLRE53@users.noreply.github.com>
2019-06-13 16:44:46 -0400

    Project 1 submission with X-tra Creidt
    Random Quote Generator with X-tra Generator

--- js/script.js
+++ js/script.js
@@ -7,136 +7,140 @@ project 1 - A Random Quote Generator
 
 
 /*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
+  Defined an Array named (quotes) of 5 quote objects all containing a 'quote'
+  and `source` property.  A 'citation` and 'year' property was added
+  to at least one object in the array. Console.log() used to log array of quotes to the console.
 ***/
-/* Defined Array of quote objects*/
 
-let  quotes = [
-  {        
-  quote: 'In prayer it is better to have a heart without words than words without a heart.',
-  source: 'John Bunyan',
-  citation: 'publication',
-  year: '1673',
+let quotes = [
+  {
+    quote: 'In prayer it is better to have a heart without words than words without a heart.',
+    source: 'John Bunyan',
+    citation: 'publication',
+    year: '1673',
 
   },
 
   {
-   quote: 'The only really happy people are those who have learned how to serve.',
-   source: 'Rick Warren',
-   citation: 'Publication',
-   year: '2000',
-   tags: "Religious"
+    quote: 'The only really happy people are those who have learned how to serve.',
+    source: 'Rick Warren',
+    citation: 'Publication',
+    year: '2000',
+    tags: "Religious"
   },
 
-  {              
-   quote: 'Complain and remain. Praise and be raised',
-   source: 'Joyce Meyer'
-   
+  {
+    quote: 'Complain and remain. Praise and be raised',
+    source: 'Joyce Meyer'
+
   },
-{
-   quote: 'Watch your thoughts, they become words. Watch your words, they become actions. Watch your actions, \
+  {
+    quote: 'Watch your thoughts, they become words. Watch your words, they become actions. Watch your actions, \
    they become habits. Watch your habits, they become your character. Watch your character, it becomes your destiny.',
-   source: 'Anonymous'
-   
-},
+    source: 'Anonymous'
 
-{
-  quote: 'When you harbor bitterness, happiness will dock elsewhere',
-  source: 'Unknown'
- 
-},
+  },
+
+  {
+    quote: 'When you harbor bitterness, happiness will dock elsewhere',
+    source: 'Unknown'
+
+  },
 
 ];
 
-//console.log(quotes);
-
 /***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Use the random number to `return` a random quote object from the `quotes` array.
+  Defined the `getRandomQuote` function to randomly select a quote within quotes array.
+  ramdom integer rounded down to whole number as quotes array is indexed by Zero(whole number)
+  Variable ranmdomQuote stores random array index number and returns random quote from `quotes` array.
 ***/
 
 function getRandomQuote() {
- 
+
   let randomQuoteIndex = Math.floor(Math.random() * quotes.length);
   let randomQuote = quotes[randomQuoteIndex];
   return randomQuote;
-
 }
- 
 
-  //console.log(getRandomQuote(quotes));
-
-
-  /***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
+/***
+   Defined `printQuote` function that calls the `getRandomQuote` function stored in quoteObjrandom
+ - Defined stringdefault to hold the HTML string while setting it equal to an empty string.  Will later
+   concatenate ther default string.
+ - Used the random quote variable to build the HtML string and added the quote and source properties
+   to the string. 
+ - if statement checked for the citation and year properteries before adding it to the HTML string.
+ - Closed the final p tag once the entire HTML string was assembled with quote properties
+ - Set the `innerHTML` of the `quote-box` div tag to the HTML String Var "stringdefault" to replaces 
+  the id value in HTML.
 ***/
 
 
-  function printQuote() {
+function printQuote() {
 
-    let quoteObjrandom = getRandomQuote();
-    let stringdefault = '';
-    
-    stringdefault += '<p class = "quote">' + quoteObjrandom.quote + '</p>';
-    stringdefault += '<p class = "source" >' + quoteObjrandom.source;
+  let quoteObjrandom = getRandomQuote();
+  let stringdefault = '';
 
-    if (quoteObjrandom.citation) {
-      stringdefault += '<span class = "citation">' + quoteObjrandom.citation + '</span>';
+  stringdefault += '<p class = "quote">' + quoteObjrandom.quote + '</p>';
+  stringdefault += '<p class = "source" >' + quoteObjrandom.source;
 
-    }
-      
-      if (quoteObjrandom.year) {
-        stringdefault += '<span class = "year">' + quoteObjrandom.year + '</span>';
-      }
-        
-       
-   if (quoteObjrandom.tags) {
+  if (quoteObjrandom.citation) {
+    stringdefault += '<span class = "citation">' + quoteObjrandom.citation + '</span>';
 
-       stringdefault += '<h3>' + quoteObjrandom.tags +'</h3>';
-   } else {
-     stringdefault += '';
+  }
+
+  if (quoteObjrandom.year) {
+    stringdefault += '<span class = "year">' + quoteObjrandom.year + '</span>';
+  }
 
 
-       } 
-   stringdefault += "</p>"; 
-   //print(message)
-console.log(stringdefault);
-document.getElementById("quote-box").innerHTML = stringdefault;
+  if (quoteObjrandom.tags) {
 
-let r = Math.floor(Math.random() *200);
-let g = Math.floor(Math.random() *200);
-let b = Math.floor(Math.random() *200);
-let randomRGB = 'rgb('+ r + ', ' + g + ', ' + b + ')';
-document.body.style.backgroundColor = randomRGB;
+    stringdefault += '<h3>' + quoteObjrandom.tags + '</h3>';
+  } else {
+    stringdefault += '';
+
+
+  }
+
+  stringdefault += "</p>";
+  
+  console.log(stringdefault);
+  document.getElementById("quote-box").innerHTML = stringdefault;
+
+  
+
+randomRGB();
 
 }
-   printQuote();
-   
+function randomRGB() {
 
-    
+
+  let r = Math.floor(Math.random() * 200);
+  let g = Math.floor(Math.random() * 200);
+  let b = Math.floor(Math.random() * 200);
+  let randomRGB = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+  document.body.style.backgroundColor = randomRGB;
+
+}
+/***auto_refresh function automatically refreshes the web page to print a new quote 
+    to the page every 20 seconds.  Initialied var fresh to store timing method and
+    passed argument function and within function invoked (function printQuote) to test 
+    a theory.***/
+
+let fresh;
+ function auto_refresh () {
+
+ fresh = window.setInterval(function(){printQuote(); randomRGB();}, 3000);
+ console.log(fresh);
+
+ }
+
 /***
   When the "Show another quote" button is clicked, the event listener 
   below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
+  function. 
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-setInterval(function(){ alert("Hello"); }, 3000);
-
+printQuote();
